c891d1711a5cfcc621bb356e003d81732c829afb
Nathaniel Bielanski <nbielans@purdue.edu>
2023-09-12 14:41:48 -0400

    secrets

--- src/secret.ts
+++ src/secret.ts
@@ -12,8 +12,8 @@
 // 5, 2 -> 1
 // 7, 4 -> 3
 // All inputs must be >= 1
-export function secret_1(_num1: number, _num2: number): number {
-    return 0;
+export function secret_1(num1: number, num2: number): number {
+    return num1%num2;
 }
 
 // Implements the following input to output mapping:
@@ -22,8 +22,8 @@ export function secret_1(_num1: number, _num2: number): number {
 // 5, 2 -> 25
 // 7, 4 -> 2401
 // All inputs must be >= 1
-export function secret_2(_num1: number, _num2: number): number {
-    return 0;
+export function secret_2(num1: number, num2: number): number {
+    return num1**num2;
 }
 
 // Implements the following input to output mapping:
@@ -32,8 +32,8 @@ export function secret_2(_num1: number, _num2: number): number {
 // 9 -> 3
 // 256 -> 16
 // All inputs must be >= 0
-export function secret_3(_num1: number): number {
-    return 0;
+export function secret_3(num1: number): number {
+    return num1**.5;
 }
 
 // Implements the following input to output mapping:
@@ -42,8 +42,15 @@ export function secret_3(_num1: number): number {
 // 5 -> 120
 // 7 -> 5040
 // All inputs must be >= 0
-export function secret_4(_num1: number): number {
-    return 0;
+export function secret_4(num1: number): number {
+    let num:number = num1--;
+
+    while(num1 > 0) {
+        num = num * num1;
+        num1--;
+    }
+    
+    return num;
 }
 
 // Implements the following input to output mapping:
@@ -52,6 +59,16 @@ export function secret_4(_num1: number): number {
 // 5 -> 5
 // 7 -> 13
 // All inputs must be >= 1
-export function secret_5(_num1: number): number {
-    return 0;
+export function secret_5(num1: number): number {
+    let a = 1;
+    let fib = 1;
+    let temp:number;
+
+    for(let i=0; i<num1-2; i++) {
+        temp = fib;
+        fib += a;
+        a = temp;
+    }
+
+    return fib;
 }
